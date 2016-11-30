c5335e0b7649e8a62d7fe296d04af1e17dc2c39f
brechen <zappeln@gmail.com>
2016-11-29 23:52:29 -0600

    create RemoteControlFragment, create onCreateView method

---
+++ app/src/main/java/net/shawnklein/android/remotecontrol/RemoteControlFragment.java
@@ -0,0 +1,37 @@
+package net.shawnklein.android.remotecontrol;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+
+public class RemoteControlFragment extends Fragment {
+    private TextView mSelectedTextView;
+    private TextView mWorkingTextView;
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup parent, Bundle savedInstanceState) {
+        View v = inflater(R.layout.fragment_remote_control, parent, false);
+
+        mSelectedTextView = (TextView)v.findViewById(R.id.fragment_remote_control_selectedTextView);
+        mWorkingTextView = (TextView)v.findViewById(R.id.fragment_remote_control_workingtextView);
+
+        View.OnClickListener numberButtonListener = new View.OnClickListener() {
+            public void onClick(View v) {
+                TextView textView = (TextView)v;
+                String working = mWorkingTextView.getText().toString();
+                String text = textView.getText().toString();
+                if (working.equals("0")) {
+                    mWorkingTextView.setText(text);
+                } else {
+                    mWorkingTextView.setText(working + text);
+                }
+            }
+        };
+
+        return v;
+    }
+}
